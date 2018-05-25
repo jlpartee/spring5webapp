ad6e598804e3209c7360eb02e877a641b1cb07e8
Johan Partee <jlpartee@me.com>
2018-05-25 13:29:28 -0500

    added publisher entity

--- src/main/java/com/jlpartee/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/jlpartee/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.jlpartee.spring5webapp.bootstrap;
 
 import com.jlpartee.spring5webapp.model.Author;
 import com.jlpartee.spring5webapp.model.Book;
+import com.jlpartee.spring5webapp.model.Publisher;
 import com.jlpartee.spring5webapp.repositories.AuthorRepository;
 import com.jlpartee.spring5webapp.repositories.BookRepository;
+import com.jlpartee.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -16,10 +18,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -28,9 +32,28 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
+
+        Publisher pubOne = new Publisher();
+        pubOne.setName("Glenview Publishing Co.");
+        pubOne.setAddress1("2500 Lehigh Ave");
+        pubOne.setAddress2("Corp Office");
+        pubOne.setCity("Glenview");
+        pubOne.setState("IL");
+        pubOne.setZipcode(60026);
+        publisherRepository.save(pubOne);
+
+        Publisher pubTwo = new Publisher();
+        pubTwo.setName("Backwaters Book Co.");
+        pubTwo.setAddress1("12944 S Commercial Ave");
+        pubTwo.setAddress2("publishing div");
+        pubTwo.setCity("Hegewisch");
+        pubTwo.setState("IL");
+        pubTwo.setZipcode(60633);
+        publisherRepository.save(pubTwo);
+
         // Eric
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", pubOne);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -39,7 +62,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", pubTwo);
         rod.getBooks().add(noEJB);
 
         authorRepository.save(rod);
--- src/main/java/com/jlpartee/spring5webapp/model/Book.java
+++ src/main/java/com/jlpartee/spring5webapp/model/Book.java
@@ -15,7 +15,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -47,11 +49,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
@@ -63,7 +65,7 @@ public class Book {
         this.authors = authors;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
 
         this.title = title;
         this.isbn = isbn;
@@ -71,7 +73,7 @@ public class Book {
         this.authors = authors;
     }
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
 
         this.title = title;
         this.isbn = isbn;
@@ -100,7 +102,7 @@ public class Book {
                 "id=" + id +
                 ", title='" + title + '\'' +
                 ", isbn='" + isbn + '\'' +
-                ", publisher='" + publisher + '\'' +
+                ", publisher='" + publisher.getName() + '\'' +
                 ", authors=" + authors +
                 '}';
     }
